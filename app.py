d430e7662fa33a64d55679ec330aa3ffbdc2619f
VaiMoto <rodrigogyn12345@gmail.com>
2026-09-14 07:26:00 -0300

    Corrige clique buscar destino

--- app.py
+++ app.py
@@ -3604,7 +3604,7 @@ async function usarMinhaLocalizacao(){
         <input id="dest_lat" type="hidden">
         <input id="dest_lon" type="hidden">
 
-        <button class="pub-btn" type="button" id="btnBuscarDestino" onclick="buscarDestino()">🔎 BUSCAR DESTINO</button>
+        <button class="pub-btn" type="button" id="btnBuscarDestino" onclick="window.buscarDestino()">🔎 BUSCAR DESTINO</button>
 
         <div id="resultado-endereco"></div>
         <div id="estimativa" class="pub-info" style="display:none"></div>
