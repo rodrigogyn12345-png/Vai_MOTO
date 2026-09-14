ea100bdd7b273b3eedb21b35fe32899f099c5a86
VaiMoto <rodrigogyn12345@gmail.com>
2026-09-14 06:35:03 -0300

    Teste de clique no botao buscar destino

--- app.py
+++ app.py
@@ -3604,7 +3604,7 @@ async function usarMinhaLocalizacao(){
         <input id="dest_lat" type="hidden">
         <input id="dest_lon" type="hidden">
 
-        <button class="pub-btn" type="button" id="btnBuscarDestino">🔎 BUSCAR DESTINO</button>
+        <button class="pub-btn" type="button" id="btnBuscarDestino" onclick="alert('TESTE DE CLIQUE OK')">🔎 BUSCAR DESTINO</button>
 
         <div id="resultado-endereco"></div>
         <div id="estimativa" class="pub-info" style="display:none"></div>
